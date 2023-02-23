d2f96a040bd4b8645aa270e0c95c4d44cbaa3fde
Dylan wall <Dylan@Dylans-MacBook-Pro.local>
2023-02-23 10:46:26 +0000

    build new JAR

--- src/main/java/com/iot/iaq/IaqApplication.java
+++ src/main/java/com/iot/iaq/IaqApplication.java
@@ -21,7 +21,7 @@ public class IaqApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://192.168.1.246:4200", "http://myiaqhub.ddns.net:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://myiaqhub.ddns.net:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
